904119f81571f6d960732c566250b9c2d581d8c7
Debora <m_deby_c@hotmail.com>
2022-07-19 19:42:28 -0300

    agregue parametros en card y home

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -1,22 +1,50 @@
 import React from 'react';
 
 
-export default function Card({tipo, modelo, precio, astillero, fabricacion, localizacion, imagen}){
-    
-    return(
+export default function Card({ tipo, Marca, modelo, Motor, precio, astillero, fabricacion, localizacion, imagen, producto, descripcion, Tamaño })
+{
+
+    return (
         <div>
-                <p>{tipo}</p>
-               <p>{modelo},</p> 
-               <p>{precio}</p> 
-               <p>{astillero}</p> 
-               <p>{fabricacion}</p> 
-               <p>{localizacion}</p> 
-               {
+            {
+                Marca ? <p>Marca: {Marca}</p> : ''
+            }
+            {
+                modelo ? <p>modelo: {modelo}</p> : ''
+            }
+            {
+                fabricacion ? <p>fabricacion: {fabricacion}</p> : ''
+            }
+            {
+                astillero ? <p>astillero: {astillero}</p> : ''
+            }
+            {
+                Motor ? <p>Motor: {Motor}</p> : ''
+            }
+            {
+                localizacion ? <p>localizacion: {localizacion}</p> : ''
+            }
+            {
+                tipo ? <p>tipo: {tipo}</p> : ''
+            }
+            {
+                precio ? <p>precio: {precio}</p> : ''
+            }
+            {
+                producto ? <p>producto: {producto}</p> : ''
+            }
+            {
+                descripcion ? <p>descripcion: {descripcion}</p> : ''
+            }
+            {
+                Tamaño ? <p>Tamaño: {Tamaño}</p> : ''
+            }
+            {
                 imagen?.map(e =>
-                <img src={e} alt='img'/>
-                    
-                    )
-               }
+                    <img src={e} alt='img' />
+                )
+            }
+
         </div>
     )
 }
