eb2e14455b167347f5b62afa1baf4e71f8ac4710
Nahuel Salomon <42101881+nahuelns@users.noreply.github.com>
2022-07-30 23:23:12 +0200

    actualizacion index.js

--- server/index.js
+++ server/index.js
@@ -1,8 +1,8 @@
 const app= require('./src/app');
-const {PORT} = require("./src/config")
+//const {PORT} = require("./src/config")
 
 
-app.listen(PORT, () =>{
+app.listen(process.env.PORT || 4000, () =>{
     console.log('server listening on port 4000')
 });
 
