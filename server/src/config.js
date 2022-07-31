94776a33e6ec8eec31cfb470f80c8a7ae886c424
Debora <m_deby_c@hotmail.com>
2022-07-31 00:57:00 -0300

    *

--- server/src/config.js
+++ server/src/config.js
@@ -9,5 +9,5 @@ module.exports = {
     MONGO_PASSWORD: process.env.MONGO_PASSWORD || "admin",
     MONGO_HOST: process.env.MONGO_HOST || "localhost",
     PORT: process.env.PORT || 4000,
-    MONGO_USERS:process.env.MONGO_USERS||`mongodb+srv://admin:admin@cluster0.mxvbffi.mongodb.net/yatesdb`,
+    MONGO_USERS:process.env.MONGO_USERS||`mongodb+srv://${MONGO_USER}:${MONGO_PASSWORD}@cluster0.mxvbffi.mongodb.net/${MONGO_DATABASE}`,
 }
