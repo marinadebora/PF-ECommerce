ac3018cf4b4c9446ce3cc5427c8925346ee63f6d
Debora <m_deby_c@hotmail.com>
2022-07-31 03:46:22 -0300

    cambio total

--- server/index.js
+++ server/index.js
@@ -1,8 +1,9 @@
 const app= require('./src/app');
-const {PORT} = require("./src/config")
+const {PORT}= require('./src/config')
 
 
-app.listen(PORT, () =>{
-    console.log('server listening on port 4000')
+
+
+app.listen( PORT, () =>{
+    console.log(`server listening on port  ${PORT}`)
 });
-
